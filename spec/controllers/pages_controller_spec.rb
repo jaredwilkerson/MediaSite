280a2967fffbc06364f0d6c594ba1f28a4e55feb
Jared Wilkerson <wilkerson.jared@gmail.com>
2011-12-06 13:01:03 -0500

    More basic improvements. New tests.
    Removed the default rails index page. Updated the css some more, Added
    tests for the rendering of the other basic pages.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,4 +6,22 @@ describe PagesController do
     get 'home'
     response.should be_successful
   end
+
+  it "should render the music page" do
+    get 'music'
+    response.should be_successful
+  end
+  it "should render the movie page" do
+    get 'movie'
+    response.should be_successful
+  end
+  it "should render the television page" do
+    get 'television'
+    response.should be_successful
+  end
+  it "should render the maintenance page" do
+    get 'maintenance'
+    response.should be_successful
+  end
+
 end
